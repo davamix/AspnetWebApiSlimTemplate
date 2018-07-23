9c5491329839319bf7b0f37668245705bab4c0a5
Daniel Valcarce <davamix@gmail.com>
2018-07-23 18:15:40 +0200

    Solution code

---
+++ Sample.Bootstrap/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Sample.Bootstrap
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore(options =>
+            {
+                options.RequireHttpsPermanent = true;
+                options.RespectBrowserAcceptHeader = true;
+            })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddFormatterMappings()
+                .AddJsonFormatters();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
